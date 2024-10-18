3dba85c5da0e1c02d02433a62b5dc48efae834ec
Milo <117958567+MiloNino@users.noreply.github.com>
2024-10-18 07:13:36 -0500

    refactorizar dia 1 (#5) (#6)

--- day1.py
+++ day1.py
@@ -1,2 +1,29 @@
-hola bebez
-Lanasauria
+
+# print("1.Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n"+
+#       "2. Knead the dough for 10 minutes.\n" +
+#       "3. Add 3g of Salt.\n" +"4. Leave to rise for 2 hours.\n" +
+#       "5. Bake at 200 degrees C for 30 minutes.")
+
+# print("Notes from Day 1")
+# print("The print statement is used to output strings")
+# print("Strings are strings of characters")
+# print("String Concatenation is done with the + sign")
+# print("New lines can be created with a \ and the letter n")
+ 
+# print("hola " + input("Como te llamas ??") + "!!")
+
+# username = input("Ingresa la palabra")
+# length= len(username)
+# print(len(username))
+# print( length)
+
+
+# glass1 = "milk"
+# glass2 = "juice"
+# print("vaso 1 "+ glass1 + " y " + "vaso 2 "+glass2 )
+# glass1,glass2= glass2,glass1
+# print("vaso 1 "+ glass1 + " y " + "vaso 2 "+glass2 )
+
+ciudad=input("nombre de tu ciudad \n") 
+mascota=input("nombre de tu mascota \n")
+print("la banda se llama: " + ciudad+mascota)
